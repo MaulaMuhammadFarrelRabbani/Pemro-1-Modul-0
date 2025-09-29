929ed3dd82fd0b6470374dfbfb1f772d168a71c8
MaulaMuhammadFarrelRabbani <farrelayey.rabbani@gmail.com>
2025-09-29 20:53:26 +0800

    Update PRAK002-2510817310008-MaulaMuhammadFarrelRabbani.c

--- PRAK002-2510817310008-MaulaMuhammadFarrelRabbani.c
+++ PRAK002-2510817310008-MaulaMuhammadFarrelRabbani.c
@@ -2,7 +2,8 @@
 
 int main(){
     printf("Selamat Pagi, Maula Muhammad Farrel Rabbani\n");
-    printf("Selamat Sore, Maula Muhammad Farrel Rabbani\n");
+    printf("Selamat Siang, Maula Muhammad Farrel Rabbani\n");
     printf("Selamat Malam, Maula Muhammad Farrel Rabbani\n");
     return 0;
+
 }
